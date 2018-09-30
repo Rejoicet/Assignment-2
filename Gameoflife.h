cbe5414511800d040f691aced71c5b4147d1ad21
Rejoicet <rejthomas@chapman.edu>
2018-09-30 07:04:59 +0000

    header file

---
+++ Gameoflife.h
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+class Corners;
+
+class Originalgrid {
+public:
+  ifstream Gameoflife;
+  string s;
+  int r, c, a, b, abc, ncount, xcount, countofx, n;
+  int *ptrr, *ptrc;
+  char cell, value;
+  char ** pdimensions;
+  char ** copypdimensions;
+  int ** count;
+
+
+  Originalgrid ();
+  char setOriginalgrid (string filename, char mode);
+  char copyOriginalgrid ();
+  char updateOriginalgrid (char mode, int output);
+  void updateCorners(Corners& corner);
+
+};
